78093e390669492b8e463062ac48cb1fd8e0d891
Alexis Canevali <alexis.canevali@gmail.com>
2019-11-25 11:16:25 +0100

    Avec les commentaires c'est mieux

--- ProgSystem/Python/Seance2/exercice1.py
+++ ProgSystem/Python/Seance2/exercice1.py
@@ -1,8 +1,11 @@
 import os,sys,random
 import multiprocessing as mp
 
+# Valeur d'arrêt des boucles
 fin = -1
 
+# Fais la somme du contenu de q
+# typeSomme definit l'affichage du résultat final
 def somme(q,typeSomme):
     somme = 0
     while True:
@@ -16,6 +19,9 @@ def somme(q,typeSomme):
     else:
         print("Somme impaire "+str(somme))
 
+# Trie une liste de nombre selon s'ils sont pairs/impairs
+# Place chiffres pairs dans queue qPair
+# Place chiffres impairs dans queue qImpair
 def triLPLI(qPair, qImpair,donnes):
     for i in donnes:
         if (i%2==0):
@@ -25,18 +31,27 @@ def triLPLI(qPair, qImpair,donnes):
     qPair.put(fin)
     qImpair.put(fin)
 
+# MAIN
+#Longueur de la liste de chiffres
 nbAlea = 10 #random.randint(0,100)
 listAlea = []
 random.seed()
+
+#Ajoute nbAlea chiffres aléatoires à la liste
 for i in range (0,nbAlea):
     listAlea.append(random.randint(0,100))
 print(listAlea)
 
+#Création des queues
 qPair = mp.Queue()
 qImpair = mp.Queue()
+
+# PROCESS DE TI
 processTri = mp.Process(target=triLPLI,args=(qPair,qImpair,listAlea))
 processTri.start()
 processTri.join()
+
+# PROCESS DE SOMME
 psSP = mp.Process(target=somme,args=(qPair,"p"))
 psSI = mp.Process(target=somme,args=(qImpair,"i"))
 
