053c934786377d1f8d1716595891df05d1d834f0
JeremyCPE <47634418+JeremyCPE@users.noreply.github.com>
2019-12-02 11:23:35 +0100

    Fin de l'exercice

--- ProgSystem/Python/Seance3/exercice1.py
+++ ProgSystem/Python/Seance3/exercice1.py
@@ -1,7 +1,6 @@
 # Juin 2019
 # Cours hippique
 # Version très basique, sans mutex sur l’écran, sans arbitre, sans annoncer le gagant, ... ...
-
 # Quelques codes d’échappement (tous ne sont pas utilisés)
 CLEARSCR="\x1B[2J\x1B[;H" # Clear SCReen
 CLEAREOS = "\x1B[J" # Clear End Of Screen
@@ -45,6 +44,10 @@ CL_WHITE="\033[01;37m" # Blanc
 import multiprocessing as mp
 import os, time,math, random, sys
 
+# Variable pour que l'utilisateur puisse parier
+PARI = "zzz" 
+ENTETE = "zzz"
+
 keep_running=True # Fin de la course ?
 global verrou
 # Une liste de couleurs à affecter aléatoirement aux chevaux
@@ -63,21 +66,63 @@ def en_rouge() : print(CL_RED,end='') # Un exemple !
 def un_cheval(ma_ligne : int) : # ma_ligne commence à 0
     col=1
     while col < LONGEUR_COURSE and keep_running :
+        verrou.acquire()
+        #Mutex ici car plusieurs process peuvent use la ressource en mm temps
         move_to(ma_ligne+1,col) # pour effacer toute ma ligne
         erase_line_from_beg_to_curs()
         en_couleur(lyst_colors[ma_ligne%len(lyst_colors)])
-        #Mutex ici car plusieurs process peuvent use la ressource en mm temps
-        verrou.acquire()
-        print('8==3')
+        if(lyst_colors[ma_ligne%len(lyst_colors)] == CL_YELLOW):
+            print(chr(ord('A')+ma_ligne)+' >8=3')
+        elif(lyst_colors[ma_ligne%len(lyst_colors)] == CL_DARKGRAY):
+            print(chr(ord('A')+ma_ligne)+'8===3')
+        else:
+            print(chr(ord('A')+ma_ligne)+'>8==3')
         verrou.release()
         col+=1
-        time.sleep(0.1 * random.randint(1,5))
-def un_arbitre(ma_ligne : int) : #Nb_process + 5
-    move_to(25,1)
-    print('En tete : ')
+        position[ma_ligne] = col
+        time.sleep(0.1 * random.randint(1,2))
+def un_arbitre(idMax : int) : #Nb_process + 5
+
+    #move_to(31,1)
+    max = position[0]
+    idMax = 0
+
+    while(1):
+        time.sleep(0.5)
+        for i in range(20):
+            if(position[i] > max):
+                max = position[i]
+                idMax = i
+                verrou.acquire()
+                move_to(Nb_process+3,0)
+                erase_line_from_beg_to_curs()
+                en_couleur(CL_WHITE)
+                print("En tete : " + chr(ord('A')+idMax))
+
+                verrou.release()
+            if(position[i] >= LONGEUR_COURSE):
+                break
+
+def faites_vos_paris(possibleValues): 
+    while(1):
+        erase_line_from_beg_to_curs()
+        data = input("Pick an answer from A to "+chr(ord('A')+possibleValues-1)+": ")
+        
+        if ord(data.upper()) not in range(ord('A'),ord('A')+possibleValues):
+            print("Not an appropriate choice.")
+        else:
+            global PARI 
+            PARI = data.upper()
+            effacer_ecran()
+            curseur_invisible()
+            move_to(Nb_process+4,0)
+            print("Votre pari :" + PARI)
+            break
+
 #−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−−
 # La partie principale :
 if __name__ == "__main__" :
+
     Nb_process=20
     mes_process = [0 for i in range(Nb_process)]
     
@@ -85,18 +130,31 @@ if __name__ == "__main__" :
     LONGEUR_COURSE = 100
     effacer_ecran()
     curseur_invisible()
-    position = mp.Array(Nb_process)
+    position = mp.Array("i",Nb_process)  #Tableau Partagé de int
+    position[:]= [0 for _ in range(Nb_process) ] #Initialisés à 0
+    idMax = 0
+
+    faites_vos_paris(Nb_process)
+
 
     for i in range(Nb_process): # Lancer Nb_process processus
         mes_process[i] = mp.Process(target=un_cheval, args= (i,))
         mes_process[i].start()
-    arbitre = mp.Process(target=un_arbitre,args=(i,)) 
+    arbitre = mp.Process(target=un_arbitre,args=(idMax,)) 
     arbitre.start()
     move_to(Nb_process+10, 1)
     print("tous lancés")
     
     for i in range(Nb_process): mes_process[i].join()
-    
+    global ENTETE
+    ENTETE = chr(ord('A')+idMax)
     move_to(24, 1)
     curseur_visible()
+    if((PARI!="zzz") and (ENTETE!="zzz")) : 
+        if(PARI==ENTETE):
+            print("VOUS AVEZ GAGNÉ LE PARI, VOUS ETES DESORMAIS MILLIONAIRE")
+        else:
+            print("LE PARI EST PERDU GROS NUL")
+    else:
+        print(str(PARI)+ " "+ str(ENTETE))
     print("Fini")
